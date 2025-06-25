22cb888b530efab0e150d75fb400d1398540169a
Mukho <mukho@kakao.com>
2025-06-26 00:32:14 +0900

    Fix: 구글 애널리틱스 설정 수정

--- src/main.js
+++ src/main.js
@@ -1,7 +1,18 @@
 import { createApp } from "vue";
 import App from "./App.vue";
+import router from "./router";
 
 import "./assets/main.css";
 
 const app = createApp(App);
+
+router.afterEach((to) => {
+  if (window.gtag) {
+    window.gtag("event", "page_view", {
+      page_path: to.fullPath,
+    });
+  }
+});
+
+app.use(router);
 app.mount("#app");
